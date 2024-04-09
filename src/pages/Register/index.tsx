7aa8c6b35b0aeb4709c234fc9a8102f40c83f3ee
e_sibuya@outlook.com <e_sibuya@outlook.com>
2024-04-09 18:59:30 -0300

    feat: Created Page Students

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../../components/NavBar"
+import RegistrationForm from "../../components/RegistrationForm"
+
+const Register = () => {
+    return (
+        <>
+            <NavBar />
+            <RegistrationForm />
+        </>
+    )
+}
+
+export default Register
---
+++ src/pages/Students/index.tsx
@@ -0,0 +1,13 @@
+import ListStudents from "../../components/ListStudents"
+import NavBar from "../../components/NavBar"
+
+const Students = () => {
+    return (
+        <>
+            <NavBar />
+            <ListStudents />
+        </>
+    )
+}
+
+export default Students
